062cbff78e0b07acd78d8dc3bd7719260e9f6342
unknown <laurab.righi@gmail.com>
2019-11-16 15:11:38 -0300

    atualização CORSS

--- SpaceX_API.py
+++ SpaceX_API.py
@@ -1,7 +1,10 @@
 import json, requests
 from flask import make_response
 from flask import Flask
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/next", methods=['GET'])
 def next():
